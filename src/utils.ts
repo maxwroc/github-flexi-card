8aa7df87e329e2688713b7ca6682b0551748baea
maxwroc <max.chodorowski@gmail.com>
2020-10-22 00:06:58 +0100

    Release v1.0.0

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 export const printVersion = () => console.info(
-    "%c GITHUB-FLEXI-CARD %c 0.5.0",
+    "%c GITHUB-FLEXI-CARD %c 1.0.0",
     "color: white; background: #cca900; font-weight: 700;",
     "color: #cca900; background: white; font-weight: 700;",
 );
