160450b3e043f46f39fbdcb76f529302cbdf0b8d
Max Chodorowski <max.chodorowski@gmail.com>
2022-02-04 19:50:57 +0000

    Show card in card picker (#30)

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,13 @@ customElements.define("github-entity", GithubEntity);
 customElements.define("github-flexi-card", GithubFlexiCard);
 
 printVersion();
+
+declare let window: HomeAssistantWindow;
+
+window.customCards = window.customCards || [];
+window.customCards.push({
+    type: "github-flexi-card",
+    name: "Github flexi card",
+    preview: true,
+    description: "Customizable card for github integration"
+});
